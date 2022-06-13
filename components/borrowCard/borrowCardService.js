bd74df6e13378e9dfa360f2b3a5c1004a1f9759f
Kiyamus <trinhngaytho69@gmail.com>
2022-06-13 23:01:58 +0700

    sort by date for borrow card

--- components/borrowCard/borrowCardService.js
+++ components/borrowCard/borrowCardService.js
@@ -50,72 +50,15 @@ exports.getBorrowedHistory = async (readerID) => {
         librarianID: { $first: "$librarianID" },
         bookName: { $push: "$bookName" },
       }
+    },
+    {
+      $sort: { createDate: -1 }
     }
   ]);
   return borrowCard;
 }
 
-// exports.getTopBorrowedBook = async (year,amount=3) => {
-//   const popularBook = await BorrowCard.aggregate([
-//     {
-//       $match:{
-//         createDate: {
-//           $gte: new Date(`${year}-01-01`),
-//           $lte: new Date(`${year}-12-31`),
-//       },
-//     },
-//     },
-//     {
-//       $unwind: "$bookBorrowed",
-//     },
-//     {
-//       $group: {
-//         _id: "$bookBorrowed",
-//         borrowedAmount: { $sum: 1 },
-//       }
-//     },
-//     {
-//       $sort: { borrowedAmount: -1 }
-//     },
-//     {
-//       $limit: amount
-//     },
-//     { $set: { bookBorrowed: { $toObjectId: "$bookBorrowed" } } },
-//     {
-//       $lookup: {
-//         from: "book",
-//         localField: "bookBorrowed",
-//         foreignField: "_id",
-//         as: "bookTitle_borrowed",
-//       },
 
-//     },
-//     {
-//       $unwind: "$bookTitle_borrowed",
-//     },
-//     {
-//       $lookup: {
-//         from: "bookTitle",
-//         localField: "bookTitle_borrowed.bookTitleID",
-//         foreignField: "bookTitleID",
-//         as: "bookTitle_borrowed",
-//       },
-//     },
-//     {
-//       $unwind: "$bookTitle_borrowed",
-//     },
-
-//     {
-//       $project: {
-//         _id: 0,
-//         bookName: "$bookTitle_borrowed.bookName",
-//         borrowedAmount: 1,
-//         bookImgage: "$bookTitle_borrowed.imageURL",
-//       }
-//     }
-//   ]);
-//   return popularBook;
-// }
 
 exports.getTopBorrowedBook = async (year, amount = 3) => {
   const popularBook = await BorrowCard.aggregate([
