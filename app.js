d09987496107612a57d963016c7284639db1674e
thidinhxm <thidinhxm@gmail.com>
2022-06-27 20:14:46 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
 
 const authRouter = require('./components/auth/authRouter');
@@ -18,6 +19,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
